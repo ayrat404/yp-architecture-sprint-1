c01f4365e52bf07dbc302c1f0ffe0ef9094c2c66
Airat Minnakhmetov <ayrat404@gmail.com>
2024-09-09 01:37:31 +0300

    sprint_1

---
+++ frontend/microfrontend/host/webpack.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    name: "host",
+    remotes: {
+        "auth": "auth@http://localhost:3001/remoteEntry.js",
+        "profile": "profile@http://localhost:3002/remoteEntry.js",
+        "places": "places@http://localhost:3003/remoteEntry.js",
+    },
+    shared: ["react", "react-dom"]
+};
